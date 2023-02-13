d003013ea6bbb4c1ab38ab37983b339eef05dc1b
Sarah Jacobs <sgjacobs14@gmail.com>
2023-02-13 13:11:15 -0500

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 const routes = require('./routes');
 
 app.use(express.json())
