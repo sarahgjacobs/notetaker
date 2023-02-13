59f6b70296fb5581b1801117f732f1e6b544c4f2
Sarah Jacobs <sgjacobs14@gmail.com>
2023-02-13 11:41:31 -0500

    updated files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const PORT = 3001;
+const PORT = 80;
 const routes = require('./routes');
 
 app.use(express.json())
